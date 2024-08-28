c61aa8f6d562be1db54a0761f8d1bbdb6c1f3265
Petrando <petrandorichard@gmail.com>
2024-08-28 16:35:33 +0800

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'r3f-image-distortion',
+        short_name: 'AppName',
+        description: 'Your app description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-icon.png',
+            sizes: '497x502',
+            type: 'image/png',
+          },          
+        ],
+      },
+    }),
+  ],
 })
